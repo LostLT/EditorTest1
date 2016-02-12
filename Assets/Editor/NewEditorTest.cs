2c007db6a735094b48ca8a1911365f587cac811b
Adam <adamdials@unity3d.com>
2016-02-12 15:37:05 -0600

    1

---
+++ Assets/Editor/NewEditorTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public static class NewEditorTest {
+
+    [Test]
+    public static void EditorTest()
+    {
+        bool test;
+
+        if (BuildTarget.iOS == BuildTarget.Android)
+            test = false;
+
+      //  return test;
+    }
+}
